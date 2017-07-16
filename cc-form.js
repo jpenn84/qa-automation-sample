601e0ec07718578abf73ef0f622be51868aef5d4
Joshua Penn <joshualeepenn@gmail.com>
2017-07-16 03:00:46 -0400

    add luhn check validation

--- cc-form.js
+++ cc-form.js
@@ -219,20 +219,39 @@ $(document).ready(function() {
         }
     }
 
+    /**
+     * Luhn algorithm in JavaScript: validate credit card number supplied as string of numbers
+     * @author ShirtlessKirk. Copyright (c) 2012.
+     * @license WTFPL (http://www.wtfpl.net/txt/copying)
+     */
+    var luhnChk = (function(arr) {
+        return function(ccNum) {
+            var
+                len = ccNum.length,
+                bit = 1,
+                sum = 0,
+                val;
+
+            while (len) {
+                val = parseInt(ccNum.charAt(--len), 10);
+                sum += (bit ^= 1) ? arr[val] : val;
+            }
+
+            return sum && sum % 10 === 0;
+        };
+    }([0, 2, 4, 6, 8, 1, 3, 5, 7, 9]));
+
     function validatePaymentPage() {
         var valid = true;
         var cvvLength;
         var ccNumLength;
+        var ccNum;
         if (ccBrand == "AMEX") {
             cvvLength = 4;
-            ccNumLength = 15 + 2;
+            ccNumLength = 15;
         } else {
             cvvLength = 3;
-            ccNumLength = 16 + 3;
-        }
-        if (document.getElementById("ccNumber").value.substring(0, 1) == "3") {
-            cvvLength = 4;
-            ccNumLength = 15 + 2;
+            ccNumLength = 16;
         }
         if (ccBrand == "-1") {
             $("#ccNumberGroup").addClass("has-error");
@@ -242,7 +261,8 @@ $(document).ready(function() {
             $("#ccNameGroup").addClass("has-error");
             valid = false;
         } else $("#ccNameGroup").removeClass("has-error");
-        if (document.getElementById("ccNumber").value.length != ccNumLength || ccBrand == "-1") {
+        ccNum = document.getElementById("ccNumber").value.replace(/ /g, '');
+        if (ccNum.length != ccNumLength || ccBrand == "-1" || !luhnChk(ccNum)) {
             $("#ccNumberGroup").addClass("has-error");
             valid = false;
         } else $("#ccNumberGroup").removeClass("has-error");
